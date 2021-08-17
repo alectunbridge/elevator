21b38f3f41806fbb864903b39dae710059a84b22
alectunbridge <alec.tunbridge@gmail.com>
2021-08-17 15:53:38 +0100

    Challenge 10

--- elevator.js
+++ elevator.js
@@ -1,30 +1,44 @@
 {
+    
     init: function(elevators, floors) {
+
         elevators.forEach(elevator => {
-            elevator.on("idle", function () { elevator.goToFloor(0); });
-
             elevator.on("floor_button_pressed", function (floorNum) { elevator.goToFloor(floorNum); });
-
-            elevator.on("")
-
-            elevator.on("passing_floor", function (floorNum, direction) {
-                if (elevator.getPressedFloors().includes(floorNum)) {
-                    elevator.goToFloor(floorNum, true);
-                }
-            });
         });
 
+        elevators[0].on("idle", function () { elevators[0].goToFloor(0); });
+        elevators[1].on("idle", function () { elevators[1].goToFloor(3); });
+        elevators[2].on("idle", function () { elevators[2].goToFloor(6); });
+        
         floors.forEach(floor => {
             floor.on("up_button_pressed", function (){
-                elevators[0].goToFloor(floor.floorNum());
+                var minDistance = 6;
+                var nearestElevator = null;
+                elevators.forEach(elevator => {
+                    var distance = Math.abs(floor.floorNum()-elevator.currentFloor());
+                    if(distance <= minDistance){
+                        minDistance = distance;
+                        nearestElevator = elevator;
+                    }
+                })
+                nearestElevator.goToFloor(floor.floorNum())
             });
 
             floor.on("down_button_pressed", function (){
-                elevators[1].goToFloor(floor.floorNum());
+                var minDistance = 6;
+                var nearestElevator = null;
+                elevators.forEach(elevator => {
+                    var distance = Math.abs(floor.floorNum()-elevator.currentFloor());
+                    if(distance <= minDistance){
+                        minDistance = distance;
+                        nearestElevator = elevator;
+                    }
+                })
+                nearestElevator.goToFloor(floor.floorNum())
             });
         });
     },
-    update: function(dt, elevators, floors) {
-        // We normally don't need to do anything here
-    }
+        update: function(dt, elevators, floors) {
+            // We normally don't need to do anything here
+        }
 }
