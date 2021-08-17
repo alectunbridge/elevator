48f38bc3a5459a41b681f450fda9e599482177f5
alectunbridge <alec.tunbridge@gmail.com>
2021-08-17 14:37:28 +0100

    Challenge 8

--- elevator.js
+++ elevator.js
@@ -5,10 +5,22 @@
 
             elevator.on("floor_button_pressed", function (floorNum) { elevator.goToFloor(floorNum); });
 
+            elevator.on("")
+
             elevator.on("passing_floor", function (floorNum, direction) {
                 if (elevator.getPressedFloors().includes(floorNum)) {
                     elevator.goToFloor(floorNum, true);
                 }
+            });
+        });
+
+        floors.forEach(floor => {
+            floor.on("up_button_pressed", function (){
+                elevators[0].goToFloor(floor.floorNum());
+            });
+
+            floor.on("down_button_pressed", function (){
+                elevators[1].goToFloor(floor.floorNum());
             });
         });
     },
