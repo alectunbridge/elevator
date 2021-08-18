7bc5c9c06a1daaabde0f7bc63f188d95474f2db0
alectunbridge <alec.tunbridge@gmail.com>
2021-08-18 09:01:39 +0100

    Challenge 9

--- elevator.js
+++ elevator.js
@@ -1,44 +1,45 @@
 {
-    
     init: function(elevators, floors) {
+        var callButtonsPressed = [];
 
         elevators.forEach(elevator => {
             elevator.on("floor_button_pressed", function (floorNum) { elevator.goToFloor(floorNum); });
+            elevator.on("passing_floor", function (floorNum, direction) {
+                if (elevator.getPressedFloors().includes(floorNum)) {
+                    elevator.goToFloor(floorNum, true);
+                }
+            });
+            elevator.on("idle", function () { elevator.goToFloor(0); });
+            elevator.on("stopped_at_floor", function (floorNum) {
+                const index = callButtonsPressed.indexOf(floorNum);
+                if (index > -1) {
+                    callButtonsPressed.splice(index, 1);
+                }
+            });
         });
 
-        elevators[0].on("idle", function () { elevators[0].goToFloor(0); });
-        elevators[1].on("idle", function () { elevators[1].goToFloor(3); });
-        elevators[2].on("idle", function () { elevators[2].goToFloor(6); });
-        
-        floors.forEach(floor => {
-            floor.on("up_button_pressed", function (){
-                var minDistance = 6;
-                var nearestElevator = null;
-                elevators.forEach(elevator => {
-                    var distance = Math.abs(floor.floorNum()-elevator.currentFloor());
-                    if(distance <= minDistance){
-                        minDistance = distance;
-                        nearestElevator = elevator;
-                    }
-                })
-                nearestElevator.goToFloor(floor.floorNum())
-            });
+        function respondToButtonPressed(floor) {
+            console.log(callButtonsPressed);
+            var minDistance = floors.length;
+            var nearestElevator = null;
+            callButtonsPressed.push(floor.floorNum())
+            elevators.forEach(elevator => {
+                var distance = Math.abs(floor.floorNum() - elevator.currentFloor());
+                if (distance <= minDistance) {
+                    minDistance = distance;
+                    nearestElevator = elevator;
+                }
+            })
+            nearestElevator.goToFloor(floor.floorNum())
+        }
 
-            floor.on("down_button_pressed", function (){
-                var minDistance = 6;
-                var nearestElevator = null;
-                elevators.forEach(elevator => {
-                    var distance = Math.abs(floor.floorNum()-elevator.currentFloor());
-                    if(distance <= minDistance){
-                        minDistance = distance;
-                        nearestElevator = elevator;
-                    }
-                })
-                nearestElevator.goToFloor(floor.floorNum())
-            });
+        floors.forEach(floor => {
+            floor.on("up_button_pressed", respondToButtonPressed);
+
+            floor.on("down_button_pressed", respondToButtonPressed);
         });
     },
-        update: function(dt, elevators, floors) {
-            // We normally don't need to do anything here
-        }
+    update: function(dt, elevators, floors) {
+        // We normally don't need to do anything here
+    }
 }
